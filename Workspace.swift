a358647ab92ad59739f5632637962e6d715ac9b7
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-04-01 10:17:51 +0900

    [#10] Tuist: Workspace.swift 추가

---
+++ Workspace.swift
@@ -0,0 +1,20 @@
+//
+//  Workspace.swift
+//  Config
+//
+//  Created by JUNHEE JO on 4/1/25.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "HoneyRoutine",
+    projects: [
+        "App/**",
+        // TODO: 해당 모듈이 추가되면 주석을 해제해야합니다.
+        // "Feature/**",
+        // "Core/**",
+        // "Domain/**",
+        // "Shared/**"
+    ]
+)
