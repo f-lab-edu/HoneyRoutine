242041513566f2f13951227c8f1d9da1a8af4956
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-04-03 19:47:01 +0900

    [#10] Tuist: ModuleTarget  app 케이스 추가

--- Tuist/ProjectDescriptionHelpers/ModuleTarget.swift
+++ Tuist/ProjectDescriptionHelpers/ModuleTarget.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 import ProjectDescription
+
 /// 모듈에서 생성될 세부 타겟 유형을 정의하는 열거형입니다.
 public enum ModuleTarget {
+    /// 실행 가능한 앱 타겟 (App의 진입점)
+    case app
+
     /// 외부 모듈에서 참조할 수 있는 인터페이스 (예: 프로토콜, DTO 등)
     case interface
 
@@ -21,7 +25,7 @@ public enum ModuleTarget {
     /// 구현 타겟에 대한 단위 테스트 타겟
     case tests
 
-    /// 스냅샷 기반의 UI 리그레션 테스트 타겟
+    /// 스냅샷 기반의 UI 테스트 타겟
     case snapshotTests
 
     /// 테스트에서 사용되는 mock, stub 등을 포함한 테스트 헬퍼 타겟
@@ -29,17 +33,30 @@ public enum ModuleTarget {
 
     public var product: Product {
         switch self {
+        case .app, .demo:
+            return .app
         case .interface, .implementation, .testing:
             return .framework
-        case .demo:
-            return .app
         case .tests, .snapshotTests:
             return .unitTests
         }
     }
 
+    public func productName(for moduleName: String) -> String? {
+        switch self {
+        case .app, .implementation:
+            return moduleName
+        case .demo:
+            return "\(moduleName)Demo"
+        default:
+            return nil
+        }
+    }
+
     public var sources: SourceFilesList {
         switch self {
+        case .app:
+            return "Sources/**"
         case .interface:
             return "Sources/Interface/**"
         case .implementation:
@@ -57,6 +74,7 @@ public enum ModuleTarget {
 
     public var nameSuffix: String {
         switch self {
+        case .app: return ""
         case .interface: return "Interface"
         case .implementation: return "Implementation"
         case .demo: return "Demo"
