3fb7ab0562d453df8673492ef8fec01a563ad403
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-04-24 16:46:19 +0900

    [#25] Feat: TimerUseCase 프로토콜 정의
    타이머의 실행 흐름을 제어하고 현재 상태를 외부로 전달하는 TimerUseCase 프로토콜을 정의하였습니다.

---
+++ Domain/TimerDomain/Sources/Interface/TimerUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  TimerUseCase.swift
+//  TimerDomainInterface
+//
+//  Created by JUNHEE JO on 4/24/25.
+//
+
+import Foundation
+import Combine
+
+/// 타이머의 실행 흐름을 제어하고 현재 상태를 스트리밍하는 UseCase 프로토콜입니다.
+public protocol TimerUseCase {
+    /// 현재 타이머 상태를 Combine Publisher로 스트리밍합니다.
+    var currentTimer: AnyPublisher<Timer, Never> { get }
+
+    /// 타이머를 처음부터 시작합니다.
+    /// - Parameter duration: 타이머의 전체 실행 시간 (초)
+    func start(duration: Int)
+
+    /// 실행 중인 타이머를 일시 정지합니다.
+    func stop()
+
+    /// 타이머를 초기 상태로 리셋합니다.
+    func reset()
+}
