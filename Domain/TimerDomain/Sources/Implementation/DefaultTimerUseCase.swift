5750b13fc926da1438fb85c3676c5ab65492b74c
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-04-24 17:26:42 +0900

    [#25] Feat: DefaultTimerUseCase 구현
    TimerUseCase 프로토콜의 기본 구현체인 DefaultTimerUseCase를 추가했습니다.

---
+++ Domain/TimerDomain/Sources/Implementation/DefaultTimerUseCase.swift
@@ -0,0 +1,69 @@
+//
+//  DefaultTimerUseCase.swift
+//  TimerDomainImplementation
+//
+//  Created by JUNHEE JO on 4/24/25.
+//
+
+import Foundation
+import Combine
+import TimerDomainInterface
+
+public final class DefaultTimerUseCase: TimerUseCase {
+
+    // MARK: - Dependencies
+    private let timerRepository: TimerRepository
+    private let timerController: TimerControllable
+
+    // MARK: - State
+    private let timerSubject = CurrentValueSubject<TimerDomainInterface.Timer?, Never>(nil)
+    private var cancellables = Set<AnyCancellable>()
+
+    public var currentTimer: AnyPublisher<TimerDomainInterface.Timer, Never> {
+        timerSubject
+            .compactMap { $0 }
+            .eraseToAnyPublisher()
+    }
+
+    // MARK: - Init
+    public init(
+        timerRepository: TimerRepository,
+        timerController: TimerControllable
+    ) {
+        self.timerRepository = timerRepository
+        self.timerController = timerController
+    }
+
+    // MARK: - UseCase Methods
+    public func start(duration: Int) {
+        let timer = Timer(duration: duration)
+
+        saveAndPublish(timer)
+        timerController.start(duration: duration)
+
+        timerController.remainingTime
+            .sink { [weak self] in
+                guard let self else { return }
+                guard let currentTimer = timerSubject.value else { return }
+                let updatedTimer = currentTimer.updateRemainingTime($0)
+                saveAndPublish(updatedTimer)
+            }
+            .store(in: &cancellables)
+    }
+
+    public func stop() {
+        timerController.stop()
+    }
+
+    public func reset() {
+        timerController.reset()
+        guard let resetTimer = timerSubject.value?.reset() else { return }
+        saveAndPublish(resetTimer)
+    }
+
+    // MARK: - Private Methods
+    private func saveAndPublish(_ timer: TimerDomainInterface.Timer) {
+        timerRepository.save(timer)
+        timerSubject.send(timer)
+    }
+}
