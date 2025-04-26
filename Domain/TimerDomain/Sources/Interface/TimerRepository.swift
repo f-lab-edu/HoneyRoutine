c0a992077c661fa2d7ef1f42c0b31bff233b5dcf
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-04-26 17:28:29 +0900

    [#25] Refact: 파일 첫 줄 공백 제거
    코드 일관성을 위해 테스트 파일 상단의 불필요한 공백을 제거했습니다.

--- Domain/TimerDomain/Sources/Interface/TimerRepository.swift
+++ Domain/TimerDomain/Sources/Interface/TimerRepository.swift
@@ -9,7 +9,6 @@ import Foundation
 
 /// 도메인 타이머(Timer)의 상태를 저장, 불러오기, 초기화하는 추상 저장소 인터페이스입니다.
 public protocol TimerRepository {
-
     /// 타이머 상태를 저장합니다.
     /// - Parameter timer: 저장할  타이머 인스턴스
     func save(_ timer: Timer)
