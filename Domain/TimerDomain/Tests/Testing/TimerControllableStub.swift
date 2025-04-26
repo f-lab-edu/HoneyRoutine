88512df49ea7026f9a794665ef10a24ad2d64a4c
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-04-26 17:26:09 +0900

    [#25] Test: DefaultTimerUseCase 테스트 코드 추가

---
+++ Domain/TimerDomain/Tests/Testing/TimerControllableStub.swift
@@ -0,0 +1,27 @@
+//
+//  TimerControllableStub.swift
+//  TimerDomainTesting
+//
+//  Created by JUNHEE JO on 4/25/25.
+//
+
+import Foundation
+import Combine
+@testable import TimerDomainInterface
+
+final class TimerControllableStub: TimerControllable {
+    private let subject = PassthroughSubject<Int, Never>()
+
+    func emitRemainingTime(_ time: Int) {
+        subject.send(time)
+    }
+
+    var remainingTime: AnyPublisher<Int, Never> {
+        subject.eraseToAnyPublisher()
+    }
+
+    func start(duration: Int) { }
+    func resume(remainingTime: Int) { }
+    func pause() { }
+    func reset() { }
+}
