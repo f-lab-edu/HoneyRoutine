1c83dbd55c302844c4869bb92a8ec973173b605f
Beepeach <eh7457303j@gmail.com>
2025-06-05 16:19:24 +0900

    [#23] Test: Timer 메서드 변경으로 인한 테스트 코드 수정
    새로운 인스턴스를 반환하는 대신 프로퍼티를 변경하는 방식으로 수정됐기때문에
    테스트 코드를 맞게 수정했습니다.

--- Domain/TimerDomain/Tests/UnitTests/TimerTests.swift
+++ Domain/TimerDomain/Tests/UnitTests/TimerTests.swift
@@ -12,94 +12,94 @@ final class TimerTests: XCTestCase {
     // MARK: - start()
     func testStart_shouldReturnTimerWithRunningTrue() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 30, isRunning: false)
+        var timer = Timer(duration: 60, remainingTime: 30, isRunning: false)
 
         // When
-        let started = timer.start()
+        timer.start()
 
         // Then
-        XCTAssertTrue(started.isRunning)
-        XCTAssertEqual(started.duration, 60)
-        XCTAssertEqual(started.remainingTime, 30)
+        XCTAssertTrue(timer.isRunning)
+        XCTAssertEqual(timer.duration, 60)
+        XCTAssertEqual(timer.remainingTime, 30)
     }
 
     // MARK: - stop()
     func testPause_shouldReturnTimerWithRunningFalse() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 10, isRunning: true)
+        var timer = Timer(duration: 60, remainingTime: 10, isRunning: true)
 
         // When
-        let stopped = timer.stop()
+        timer.stop()
 
         // Then
-        XCTAssertFalse(stopped.isRunning)
+        XCTAssertFalse(timer.isRunning)
     }
 
     func testPause_shouldPreserveRemainingTime() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 10, isRunning: true)
+        var timer = Timer(duration: 60, remainingTime: 10, isRunning: true)
 
         // When
-        let stopped = timer.stop()
+        timer.stop()
 
         // Then
-        XCTAssertEqual(stopped.remainingTime, 10)
+        XCTAssertEqual(timer.remainingTime, 10)
     }
 
     // MARK: - reset()
     func testReset_shouldReturnTimerWithRemainingTimeEqualsDuration() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 15, isRunning: true)
+        var timer = Timer(duration: 60, remainingTime: 15, isRunning: true)
 
         // When
-        let reset = timer.reset()
+        timer.reset()
 
         // Then
-        XCTAssertEqual(reset.remainingTime, 60)
+        XCTAssertEqual(timer.remainingTime, 60)
     }
 
     func testReset_shouldReturnTimerWithRunningFalse() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 15, isRunning: true)
+        var timer = Timer(duration: 60, remainingTime: 15, isRunning: true)
 
         // When
-        let reset = timer.reset()
+        timer.reset()
 
         // Then
-        XCTAssertFalse(reset.isRunning)
+        XCTAssertFalse(timer.isRunning)
     }
 
     // MARK: - updateRemainingTime()
     func testUpdateRemainingTime_shouldClampAboveMaxToDuration() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 30)
+        var timer = Timer(duration: 60, remainingTime: 30)
 
         // When
-        let updated = timer.updateRemainingTime(100)
+        timer.updateRemainingTime(100)
 
         // Then
-        XCTAssertEqual(updated.remainingTime, 60)
+        XCTAssertEqual(timer.remainingTime, 60)
     }
 
     func testUpdateRemainingTime_shouldClampBelowMinToZero() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 30)
+        var timer = Timer(duration: 60, remainingTime: 30)
 
         // When
-        let updated = timer.updateRemainingTime(-10)
+        timer.updateRemainingTime(-10)
 
         // Then
-        XCTAssertEqual(updated.remainingTime, 0)
+        XCTAssertEqual(timer.remainingTime, 0)
     }
 
     func testUpdateRemainingTime_shouldSetExactValueWithinBounds() {
         // Given
-        let timer = Timer(duration: 60, remainingTime: 30)
+        var timer = Timer(duration: 60, remainingTime: 30)
 
         // When
-        let updated = timer.updateRemainingTime(25)
+        timer.updateRemainingTime(25)
 
         // Then
-        XCTAssertEqual(updated.remainingTime, 25)
+        XCTAssertEqual(timer.remainingTime, 25)
     }
 }
